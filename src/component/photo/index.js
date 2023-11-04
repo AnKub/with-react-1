e97a3c98b193e1c722513b3057759e817537f66d
AnKub <ankub3088@gmail.com>
2023-11-04 13:06:27 +0200

    with gif

--- src/component/photo/index.js
+++ src/component/photo/index.js
@@ -2,7 +2,7 @@ import './index.css';
 
 export default function Photo({src, name}) {
 return (
-<div>
+<div className='rth'>
 <img src={src} alt ={name} className='photo'/>;
 <img className="attrac__fun" alt="boo" src="https://i.giphy.com/media/hZkeJc5vrYpksAG5V0/giphy.webp" width={200}/>
 </div>
